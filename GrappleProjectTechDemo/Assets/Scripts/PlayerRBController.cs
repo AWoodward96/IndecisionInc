fcb624dba5c72499797faea7217f6c14ab9592ba
tyler13731 <tylerac@rochester.rr.com>
2016-10-02 22:04:39 -0400

    Jetpack Cooldown
    Added a simple cooldown system to the jetpack (its not really a jetpack
    btw, more of a pulsepack). It's indicated by a slider at the bottom
    left. I wanted to make the slider change color when the pack can't be
    used, but that doesn't seem to work the way I'm used to.

--- GrappleProjectTechDemo/Assets/Scripts/PlayerRBController.cs
+++ GrappleProjectTechDemo/Assets/Scripts/PlayerRBController.cs
@@ -1,46 +1,77 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerRBController : MonoBehaviour {
+public class PlayerRBController : MonoBehaviour
+{
 
     public float gravity;
     Rigidbody2D myRigidbody;
     public float jumpForce;
-	// Use this for initialization
-	void Start () {
+    public GameObject jetpackSlider;
+    private int jetpackCooldown;
+
+    // Use this for initialization
+    void Start()
+    {
         myRigidbody = GetComponent<Rigidbody2D>();
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        jetpackCooldown = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Physics.gravity = new Vector3(0, gravity, 0);
-        handleInput(); 
-	}
+        handleInput();
+    }
+
+    void FixedUpdate()
+    {
+        if (jetpackCooldown < 100)
+        {
+            jetpackCooldown += 1;
+        }
+        jetpackSlider.GetComponent<Slider>().value = jetpackCooldown;
+        if (jetpackCooldown < 30)
+        {
+            jetpackSlider.GetComponent<Slider>().image.color = Color.red;
+        }
+        else
+        {
+            jetpackSlider.GetComponent<Slider>().image.color = Color.cyan;
+        }
+    }
 
 
     void handleInput()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if (jetpackCooldown >= 30)
         {
-            myRigidbody.AddForce(new Vector2(0, jumpForce));
-        }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                myRigidbody.AddForce(new Vector2(0, jumpForce));
+                jetpackCooldown -= 30;
+            }
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            myRigidbody.AddForce(new Vector2(-jumpForce, 0));
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            myRigidbody.AddForce(new Vector2(0,-jumpForce));
-        }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                myRigidbody.AddForce(new Vector2(-jumpForce, 0));
+                jetpackCooldown -= 30;
+            }
 
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            myRigidbody.AddForce(new Vector2(jumpForce, 0));
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                myRigidbody.AddForce(new Vector2(0, -jumpForce));
+                jetpackCooldown -= 30;
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                myRigidbody.AddForce(new Vector2(jumpForce, 0));
+                jetpackCooldown -= 30;
+            }
         }
     }
 
