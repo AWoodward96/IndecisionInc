e0057def72f23360f4a72048639949bd2dccfd51
venku122 <tarazevits@gmail.com>
2016-10-08 22:24:19 -0400

    Added four emitters for Jetpack particle effects

--- GrappleProjectTechDemo/Assets/Scripts/PlayerRBController.cs
+++ GrappleProjectTechDemo/Assets/Scripts/PlayerRBController.cs
@@ -11,6 +11,11 @@ public class PlayerRBController : MonoBehaviour
     public Slider jetpackSlider;
     private int jetpackCooldown;
 
+    public ParticleSystem emitterUp;
+    public ParticleSystem emitterDown;
+    public ParticleSystem emitterLeft;
+    public ParticleSystem emitterRight;
+
     // Just in case we need it
     public GameObject GrapplingHookPrefab;
     GrappleProjectile grapplingHook;
@@ -22,7 +27,13 @@ public class PlayerRBController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        
         myRigidbody = GetComponent<Rigidbody2D>();
+        /*
+        emitterUp    = GameObject.Find("up_facing_jet").GetComponent<ParticleSystem>();
+        emitterDown  = GameObject.Find("down_facing_jet").GetComponent<ParticleSystem>();
+        emitterLeft  = GameObject.Find("left_facing_jet").GetComponent<ParticleSystem>();
+        emitterRight = GameObject.Find("right_facing_jet").GetComponent<ParticleSystem>();*/
         jetpackCooldown = 100;
 
         GameObject grapple = GameObject.FindGameObjectWithTag("GrapplingHook");
@@ -72,14 +83,17 @@ public class PlayerRBController : MonoBehaviour
     {
         if (jetpackCooldown >= 30)
         {
+
             if (Input.GetKeyDown(KeyCode.W))
             {
+                emitterDown.enableEmission = true;
                 myRigidbody.AddForce(new Vector2(0, jumpForce));
                 jetpackCooldown -= 30;
             }
 
             if (Input.GetKeyDown(KeyCode.A))
             {
+                emitterRight.enableEmission = true;
                 myRigidbody.AddForce(new Vector2(-jumpForce, 0));
                 jetpackCooldown -= 30;
             }
@@ -87,6 +101,7 @@ public class PlayerRBController : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.S))
             {
+                emitterUp.enableEmission = true;
                 myRigidbody.AddForce(new Vector2(0, -jumpForce));
                 jetpackCooldown -= 30;
             }
@@ -94,9 +109,19 @@ public class PlayerRBController : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.D))
             {
+                emitterLeft.enableEmission = true;
                 myRigidbody.AddForce(new Vector2(jumpForce, 0));
                 jetpackCooldown -= 30;
             }
+
+            if (!Input.anyKey)
+            {
+                emitterLeft.enableEmission = false;
+                emitterUp.enableEmission = false;
+                emitterRight.enableEmission = false;
+                emitterDown.enableEmission = false;
+            }
+            
         }
     }
 
