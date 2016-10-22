98c92b800ccb1a53aae9c269593f13d794ee9e31
Alexander <alex.woodward96@gmail.com>
2016-10-22 14:40:06 -0400

    Added overlay textures (which requires a script to run) Messing with block coloring

---
+++ GrappleProjectTechDemo/Assets/Scripts/ProperTilingScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProperTilingScript : MonoBehaviour {
+
+    // A symple script that modifies the textures tile values based on the current scale
+    SpriteRenderer myRenderer;
+    public float modifier;
+
+    Material myMat;
+    void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+        if(myRenderer)
+            myMat = myRenderer.material;
+
+        if (modifier == 0)
+            modifier = 1;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if(myMat)
+        {
+            myMat.mainTextureScale = transform.parent.lossyScale / modifier;
+        }
+	}
+}
